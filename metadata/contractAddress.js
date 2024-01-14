b1e8444cf31fa499312e44f9db7a21aaf3f7aec4
Mohd Shahzil <Mohdshahzil59@gmail.com>
2024-01-14 12:53:23 +0530

    Challenge given by metacrafter

---
+++ metadata/contractAddress.js
@@ -0,0 +1,1 @@
+export const nftAddress = "0x280a80ECa2b2378f33A4f2D2758d58466c25df71";
---
+++ scripts/getBalance.js
@@ -0,0 +1,26 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
+
+const tokenAddress = "0x280a80ECa2b2378f33A4f2D2758d58466c25df71"; 
+const tokenABI = tokenContractJSON.abi;
+const walletAddress = "0xB72c286F1fD6a8121466F3b611819d4b0afC00A6"; 
+
+async function main() {
+
+    const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
+
+    console.log("You now have: " + await token.balanceOf(walletAddress) + " tokens");
+  }
+  
+  // We recommend this pattern to be able to use async/await everywhere
+  // and properly handle errors.
+  main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
